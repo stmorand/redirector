cf348bcc05a29ab41987897401c284da16d2f0af
Stephan <stephan@morand.pro>
2023-08-04 14:55:00 +0200

    Add a log file

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ $json_data = json_decode($json,true);
 // get a random link from the file
 $random_url = $json_data["links"][rand(0, ( count($json_data["links"]) - 1) )]["URL"];
 
+// log access
+$handle = fopen("logs.csv", "a");
+$access = isset($_GET["a"]) ? $_GET["a"] : "direct";
+$line = [ date("Y-m-d H:i:s"), $access ,$_SERVER["REMOTE_ADDR"], $_SERVER["HTTP_REFERER"],$_SERVER["HTTP_USER_AGENT"],$random_url];
+fputcsv($handle, $line,";");
+fclose($handle);
+
 // redirect to this link
 header('Location: '.$random_url);
 
